c9f29a35bb1c40e91255512b3de74841c6df1ccc
Lizmary Chardon <lizmary0209@gmail.com>
2026-01-26 00:49:28 -0500

    Redesign header and add SVG logo with accessible alt text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,38 @@
+import logo from "../../assets/logo.svg";
 import "./Header.css";
 
-function Header({ onOpenLogin, onOpenRegister }) {
+function Header({ isLoggedIn, onOpenLogin, onOpenRegister, onLogout }) {
     return (
         <header className="header">
-            <h1 className="header__logo">Reflect</h1>
-
-            <div className="header__actions">
-                <button className="header__button" type="button" onClick={onOpenLogin}>
-                    Sign in
-                </button>
-                <button className="header__button header__button_primary" type="button" onClick={onOpenRegister}>
-                    Sign up
-                </button>
+            <div className="header__inner">
+                <div className="header__brand">
+                    <img src={logo} alt="Reflect logo with three green leaves" 
+                    className="header__logo"/>
+                        <h1 className="header__title">Reflect</h1>
             </div>
-        </header>
+
+<div className="header__actions">
+    {!isLoggedIn ? (
+        <>
+        <button className="header__button" type="button" onClick={onOpenLogin}>
+            Sign in
+        </button>
+        <button
+        className="header__button header__button_primary"
+        type="button"
+        onClick={onOpenRegister}
+        >
+            Sign up
+        </button>
+        </>
+    ) : (
+        <button className="header__button" type="button" onClick={onLogout}>
+            Logout
+        </button>
+    )}
+</div>
+</div>
+</header>
     );
 }
 
